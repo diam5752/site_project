2248ea1bc58dc38c52799e7f689d50b6e726b092
diam5752 <diam5752@gmail.com>
2018-07-12 02:28:04 +0300

    80% done , needs some micro configurations

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Bootstrap 4 Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
+
+<body>
+
+<div class="jumbotron jumbotron-fluid">
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-md navbar-dark ">
+            <div class="d-flex w-50 order-0">
+                <a class="navbar-brand mr-1" href="#"> <img src="icons/logo.png"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+            </div>
+            <div class="navbar-collapse collapse justify-content-center order-2" id="collapsingNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">
+                            <h4 class="text-white">Codeply</h4> <span class="sr-only">Home</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="//codeply.com">
+                            <h4 class="text-white">Codeply</h4>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <h4 class="text-white">Codeply</h4>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <span class="navbar-text small text-truncate mt-1 w-50 text-right order-1 order-md-last"></span>
+        </nav>
+        <br><br>
+        <div>
+            <img src="icons/videoblog.png" class="mx-auto d-block">
+            <h4 class="text-center text-white">LE NEWS SUL MONDO DEL MATRIMONIO</h4>
+        </div>
+    </div>
+</div>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-1">
+            <img src="icons/monda_sposa.png">
+        </div>
+        <div class="col-sm-11">
+            <div class="form-group">
+                <div>
+                    <div class="float-right">
+                        <button type="button" class="btn btn-info">Info</button>
+                    </div>
+                    <div class="float-right">
+                        <input type="text" class="form-control ">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+</div>
+
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-2"> </div>
+        <div class="col-sm-5">
+            <h2>RED CARPET INSPIRATION</h2>
+            <br>
+            <img class="img-fluid" src="icons/redcarpet.png">
+            <p> <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+            </p>
+
+            <img src="icons/line.png">
+            <span class="text-right  small text-muted"> CONDIVIDI QUESTO ARTICOLO</span>
+            <img style="float: right; margin: 0px 0px 15px 15px;" src="icons/pinterest_icon.png">
+            <img style="float: right; margin: 0px 0px 15px 15px;" src="icons/tweeter_icon.png">
+            <img style="float: right; margin: 0px 0px 15px 15px;" src="icons/google_icon.png">
+            <img style="float: right; margin: 0px 0px 15px 15px;" src="icons/facebook_icon.png">
+
+        </div>
+        <div class="col-sm-3 border border-primary ">
+            <br>
+            <h6 class="text-left small text-muted">ULTIMI VIDEO PUBLICATI</h6>
+            <h2 class="text-center">whatisleft</h2>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-1">
+                    <img src="icons/video_corellati_icon.png">
+                </div>
+            </div>
+            <br>
+        </div>
+    </div>
+
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-2"> </div>
+        <div class="col-sm-2">
+            <img src="icons/thumbnail_example.png" class="img-thumbnail">
+            <p> SUMMER FASHION 2018</p>
+        </div>
+        <div class="col-sm-2">
+            <img src="icons/thumbnail_example.png" class="img-thumbnail">
+            <p> BAGS SPECIAL</p>
+        </div>
+    </div>
+</div>
+
+
+</body>
+
+</html>
