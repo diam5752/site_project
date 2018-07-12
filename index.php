f174ebbdb10e0d114572dbddc56d70657f686daa
diam5752 <diam5752@gmail.com>
2018-07-12 23:27:30 +0300

    front end done

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Bootstrap 4 Example</title>
+    <title>site project</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
@@ -14,71 +14,73 @@
 
 <body>
 
-<div class="jumbotron jumbotron-fluid">
-    <div class="container-fluid">
-        <nav class="navbar navbar-expand-md navbar-dark ">
-            <div class="d-flex w-50 order-0">
-                <a class="navbar-brand mr-1" href="#"> <img src="icons/logo.png"></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-md navbar-dark" style="position:relative;top:-15px;">
+                <div class="d-flex w-50 order-0">
+                    <a class="navbar-brand mr-1" href="#" style="position:relative;top:-15px;left:17px; "> <img src="icons/logo.png"></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+                <div class="navbar-collapse collapse justify-content-center order-2" id="collapsingNavbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">
+                                <p class="text-white" style="font-size:17px;white-space: nowrap;"> V. DIGITALE </p> </a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">
+                                <p class="text-white" style="font-size:17px;white-space: nowrap;">V. CARTECEA</p> </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="//codeply.com">
+                                <p class="text-white" style="font-size:17px;white-space: nowrap;" >CONTENUTO</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <p class="text-white" style="font-size:17px;white-space: nowrap;">EDIZIONI PASSATE</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <span class="navbar-text small text-truncate mt-1 w-50 text-right order-1 order-md-last"></span>
+            </nav>
+
+            <div>
+                <img src="icons/videoblog.png" class="mx-auto d-block" style="position: relative;bottom:-50px;">
+                <h4 class="text-center text-white" style="font-size:14px;position: relative;bottom:-55px;" >LE NEWS SUL MONDO DEL MATRIMONIO</h4>
             </div>
-            <div class="navbar-collapse collapse justify-content-center order-2" id="collapsingNavbar">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">
-                            <h4 class="text-white">Codeply</h4> <span class="sr-only">Home</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="//codeply.com">
-                            <h4 class="text-white">Codeply</h4>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <h4 class="text-white">Codeply</h4>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <span class="navbar-text small text-truncate mt-1 w-50 text-right order-1 order-md-last"></span>
-        </nav>
-        <br><br>
-        <div>
-            <img src="icons/videoblog.png" class="mx-auto d-block">
-            <h4 class="text-center text-white">LE NEWS SUL MONDO DEL MATRIMONIO</h4>
         </div>
     </div>
-</div>
 
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-1">
+                <img src="icons/monda_sposa.png">
+            </div>
+            <div class="col-sm-11">
+                <div class="form-group">
+                    <div>
+                        <div class="float-right">
+                            <button type="button" class="btn btn-info">Info</button>
+                        </div>
+                        <div class="float-right">
+                            <input type="text" class="form-control ">
+                        </div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-sm-1">
-            <img src="icons/monda_sposa.png">
-        </div>
-        <div class="col-sm-11">
-            <div class="form-group">
-                <div>
-                    <div class="float-right">
-                        <button type="button" class="btn btn-info">Info</button>
                     </div>
-                    <div class="float-right">
-                        <input type="text" class="form-control ">
-                    </div>
-
                 </div>
             </div>
         </div>
+        <br>
     </div>
-    <br>
-</div>
-
 
 <div class="container-fluid">
     <div class="row">
         <div class="col-sm-2"> </div>
-        <div class="col-sm-5">
+        <div class="col-sm-5" style="margin-right:30px;">
             <h2>RED CARPET INSPIRATION</h2>
             <br>
             <img class="img-fluid" src="icons/redcarpet.png">
@@ -93,10 +95,60 @@
             <img style="float: right; margin: 0px 0px 15px 15px;" src="icons/facebook_icon.png">
 
         </div>
-        <div class="col-sm-3 border border-primary ">
-            <br>
+
+        <div class="col-sm-4">
+            <br><br>
             <h6 class="text-left small text-muted">ULTIMI VIDEO PUBLICATI</h6>
-            <h2 class="text-center">whatisleft</h2>
+            <br>
+
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="icons/camera_example_icon.png" style="height:72px;width=:85px;">
+                </div>
+                <div class="col-sm-4"> <br>
+                    <h6 class="small"><b> LE INTERVISTE AI PROTAGONISTI... </b> </h6>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="icons/girlsShoping_example_icons.png" style="height:72px;width=:85px;">
+                </div>
+                <div class="col-sm-4"> <br>
+                    <h6 class="small"><b> LE INTERVISTE AI PROTAGONISTI... </b> </h6>
+                </div>
+                <br>
+            </div>
+
+            <br>
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="icons/food_example_icon.png">
+                </div>
+                <div class="col-sm-4"> <br>
+                    <h6 class="small"><b> LE INTERVISTE AI PROTAGONISTI... </b> </h6>
+                </div>
+                <br>
+            </div>
+
+            <br><br>
+            <h6 class="text-left small text-muted">CATEGORIE</h6>
+            <br>
+            <ul style="position: relative; right: 20px; font: italic bold 12px/30px Georgia, serif;">
+                <li>Sposa Collezioni</li>
+                <li>Sposo Collezioni</li>
+                <li>Moda Donna</li>
+                <li>Moda Uomo</li>
+                <li>Accessori</li>
+                <li>Real Wedding</li>
+                <li>Ispirazioni</li>
+                <li>Make Up</li>
+                <li>Hairstyle</li>
+                <li>Benessere</li>
+                <li>Home</li>
+                <li>Viaggi</li>
+                <li>Eventi</li>
+            </ul>
         </div>
 
         <div class="container">
@@ -108,23 +160,62 @@
             <br>
         </div>
     </div>
-
 </div>
 
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-sm-2"> </div>
-        <div class="col-sm-2">
-            <img src="icons/thumbnail_example.png" class="img-thumbnail">
-            <p> SUMMER FASHION 2018</p>
-        </div>
-        <div class="col-sm-2">
-            <img src="icons/thumbnail_example.png" class="img-thumbnail">
-            <p> BAGS SPECIAL</p>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-2"> </div>
+            <div class="col-sm-2">
+                <img src="icons/thumbnail_example.png" class="img-thumbnail">
+                <p> SUMMER FASHION 2018</p>
+            </div>
+            <div class="col-sm-2">
+                <img src="icons/bag_example_icon.png" class="img-thumbnail">
+                <p> BAGS SPECIAL</p>
+            </div>
         </div>
     </div>
-</div>
 
+    <br><br><br><br>
+
+<div>
+        <footer class="page-footer font-small white pt-4" style="background-color:#343434">
+
+    <div class="container">
+
+        <div class="row">
+        <div class="col-sm-2">
+                <img src="icons/miasposa_white_icon.png" class="img-responsive">
+        </div>
+        <div class="col-sm-2"> </div>
+            <div class="col-sm-4">
+                <img src="icons/pink_logo_icon2.png" style="">
+                <img src="icons/si_logo_icon.png">
+            </div>
+            <div class="col-sm-4 ">
+                <img src="icons/info_icons.png" class="float-right">
+            </div>
+        </div>
+        <br>
+
+    </div>
+
+
+    <div class=" text-left py-2 container-fluid" style="background-color:#000000;color:white;">
+        <div class="container">
+        <div class="row">
+
+       <p class="col-sm-5" > <small>New Events srl - P.Iva 04042850612 - Via Nobel, Aversa (prov. di Caserta) </small> </p>
+            <div class="col-sm-7" align="right">
+            <h5 > io design agency </h5>
+                </div>
+        </div>
+            </div>
+    </div>
+
+  </footer>
+
+    </div>
 
 </body>
 
